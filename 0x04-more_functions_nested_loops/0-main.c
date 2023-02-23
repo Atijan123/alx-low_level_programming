af01d2ef2626f710d374fce9bbcbc8e8a77b4e12
Ahmed <ahmedrammeh24@gmail.com>
2023-02-23 14:50:27 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int c;
+
+    c = 'A';
+    printf("%c is uppercase: %d\n", c, _isupper(c));
+
+    c = 'a';
+    printf("%c is uppercase: %d\n", c, _isupper(c));
+
+    return (0);
+}
+
